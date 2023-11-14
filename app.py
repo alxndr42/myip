37fee186801d45466ddcf9b4cdf605acb1942f9b
Alexander Dietrich <alexander@dietrich.cx>
2023-11-14 23:47:02 +0100

    Add JSON response support

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 
 from flask import Flask, request
@@ -9,8 +10,30 @@ if os.environ.get('MYIP_PROXY') == 'true':
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 
+class ResponseType(Enum):
+    """Supported response content types."""
+    TEXT = 1
+    JSON = 2
+    HTML = 3
+
+
 @app.route('/')
 def root():
-    body = f'{request.remote_addr}\n'
-    headers = {'Content-Type': 'text/plain'}
-    return (body, headers)
+    response_type = get_response_type()
+    if response_type == ResponseType.JSON:
+        return {'ip': request.remote_addr}
+    else:
+        body = f'{request.remote_addr}\n'
+        headers = {'Content-Type': 'text/plain'}
+        return (body, headers)
+
+
+def get_response_type():
+    """Return the preferred ResponseType."""
+    for item in request.accept_mimetypes:
+        if item[0] == 'text/html':
+            return ResponseType.HTML
+        if item[0] == 'application/json':
+            return ResponseType.JSON
+    else:
+        return ResponseType.TEXT
